8ba065ba7c4f9fd3c98e121026f3acbc2876c4c3
Sam <sam@thesamgordon.com>
2025-12-06 00:12:01 -0500

    Update entrypoint.sh

--- haproxy/entrypoint.sh
+++ haproxy/entrypoint.sh
@@ -11,6 +11,6 @@ precedence ::ffff:0:0/96  10
 precedence ::/0           100
 EOF
 
-envsubst '${PATRONI1_IP} ${PATRONI2_IP} ${PATRONI3_IP}' < /usr/local/etc/haproxy/haproxy.cfg.template > /usr/local/etc/haproxy/haproxy.cfg
+envsubst '${PATRONI1_IP} ${PATRONI2_IP} ${PATRONI3_IP}' < /usr/local/etc/haproxy/haproxy.template.cfg > /usr/local/etc/haproxy/haproxy.cfg
 
 exec haproxy -f /usr/local/etc/haproxy/haproxy.cfg
